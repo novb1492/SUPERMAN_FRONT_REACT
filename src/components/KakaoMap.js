249587d8229873c53807817646152f1433797088
김준영 <novb1492@naver.com>
2022-09-06 16:19:34 +0900

    recoil익히고 하자 redux 직렬화 떄문이면

--- src/components/KakaoMap.js
+++ src/components/KakaoMap.js
@@ -1,21 +1,31 @@
-const { useEffect, useState } = require("react")
-
+const { useEffect, useState, useRef } = require("react")
+/**
+ * 
+ * @param {*} param0 
+ * @returns 
+ */
 function KakaoMap({idName,width,height,reWidth,reHeight,lat,lon}) {
     const[map,setMap]=useState(null);
+    const kMap = useRef();
+
     useEffect(()=>{
-        var container = document.getElementById(idName);
-        var options = {
+        let container = document.getElementById(idName);
+        let options = {
           center: new window.kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
           level: 3
         };
-        var map = new window.kakao.maps.Map(container, options);
-        setMap(map);
+        let mapInit = new window.kakao.maps.Map(container, options);
+        window.addEventListener('resize',resize);
+        setMap(mapInit);
+
     },[]);
-    function getMap() {
-        return map;
+    function resize() {
+        var domMap=kMap.current;
+        domMap.style.width=window.innerWidth+'px';
+        domMap.style.height=window.innerHeight+'px';
     }
     return(
-        <div id={idName} style={{width:width+"px", height:height+"px"}}></div>
+        <div id={idName} ref={kMap} style={{width:width+"px", height:height+"px"}}></div>
     )
 }
 export default KakaoMap;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -4,7 +4,7 @@ function Main() {
     return(
         <div>
             main
-            <KakaoMap idName={'map'} height={500} width={1000} key={'mainMap'}></KakaoMap>
+            <KakaoMap idName={'map'} height={500} width={1000} key={'mainMap'} reHeight={100} reWidth={500}></KakaoMap>
         </div>
     )
 }
