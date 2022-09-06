fbbe5eebaba639e2b9188314234beb372bc1d729
김준영 <novb1492@naver.com>
2022-09-06 13:52:11 +0900

    절대경로 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
+import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
+import MainPage from "pages/MainPage";
 function App() {
   return (
-    <div className="App">
-      hello
-        
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage></MainPage>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/MainPage.js
@@ -0,0 +1,9 @@
+function Main() {
+    return(
+        <div>
+            main
+        </div>
+    )
+}
+
+export default Main;
